609b36b04a2915ac6826b207aabfa0b609556ff6
Philippe Aucazou <paucazou@yahoo.fr>
2020-02-22 04:33:19 +0100

    Added questions

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import random
 import time
 import util
 
+
 def ask():
     """Ask one random question to user"""
     question = get_qna()
     start = time.time()
     answer = input(question[0]+' ')
     answer = [ x[0].upper() + x[1:] for x in answer.split()]
-    if set(answer) != set(question[1]()):
+    if check_answer(answer,question[1]):
         print("Error")
         print(question[1]())
     else:
@@ -22,11 +23,29 @@ def ask():
     end = time.time()
     print(f"Time: {end-start} seconds.")
 
+def check_answer(answer,answer_expected) -> bool:
+    """Check the validity of an answer"""
+    try:
+        return set(answer) != set(answer_expected)
+    except TypeError:
+        return answer == answer_expected
+
 
 def get_qna():
+    # random data
     key = util.get_random_key()
+    function = random.choice(util.functions)
+    note = random.choice(util.find_degree(key,*range(1,8)))
+    degree = random.choice(range(1,8))
+
     questions = {
             f"What are the most important degrees in {key}?":lambda : util.find_degree(key,1,4,5),
+            f"What is the {function} in {key}?":lambda : util.find_function(key,function),
+            f"What is the degree of {note} in {key}?":lambda : util.get_degree_of(key,note),
+            f"What is {degree} in {key}?":lambda : util.find_degree(key,degree),
+            f"How many accidentals for {key}?":lambda : len(util.get_accidentals(key)),
+            f"What is the key signature of {key}?":lambda : util.get_accidentals(key),
+            f"What are the accidentals not in the key signature of {key}?":lambda : util.get_minor_accidentals(key) if key.islower() else "",
             }
     return random.choice(list(questions.items()))
 
--- util.py
+++ util.py
@@ -7,6 +7,14 @@ import mingus.core.keys as keys
 import mingus.core.scales as scales
 import random
 import string
+functions = """tonic
+supertonic
+mediant
+subdominant
+dominant
+submediant
+leadingtone
+""".split()
 
 def get_random_key():
     """Get a random key, major or minor"""
@@ -15,11 +23,35 @@ def get_random_key():
     tonics = itertools.chain.from_iterable([(n,n+'b',n+'#') for n in tonics])
     return random.choice(list(tonics))
 
+def get_scale(key):
+    """Return a scale, major or minor,
+    matching with key"""
+    return scales.Major(key) if key.isupper() else scales.HarmonicMinor(key.upper())
+
+def get_accidentals(key):
+    """Return every note with accidental
+    in key"""
+    scale = get_scale(key)
+    return [n for n in scale.ascending() if ('b' in n or '#' in n)]
+
+def get_minor_accidentals(key):
+    """Return the sixth and seventh degrees
+    augmented in minor scale key"""
+    scale = scales.MelodicMinor(key)
+    return scale.degree(6), scale.degree(7)
+
+def get_degree_of(key,note):
+    """Get the degree of note
+    in key"""
+    scale = get_scale(key)
+    return scale.ascending().index(note)
+
+
 def find_degree(key,*d):
     """Find the degrees of the scale of the key
     d is a sequence of integer from 1 to 7
     """
-    scale = scales.Major(key) if key.isupper() else scales.HarmonicMinor(key.upper())
+    scale = get_scale(key)
     return [scale.degree(i) for i in d]
 
 def find_function(scale,*f):
@@ -34,15 +66,6 @@ def find_function(scale,*f):
         - submediant
         - leadingtone
         """
-    functions = """none
-    tonic
-    supertonic
-    mediant
-    subdominant
-    dominant
-    submediant
-    leadingtone
-    """.split()
-    return find_degree(scale,*[functions.index(n) for n in f])
+    return find_degree(scale,*[functions.index(n)+1 for n in f])
 
 

1e4aa69f0ba832c1b1eacb1003353c2769d3d313
Philippe Aucazou <paucazou@yahoo.fr>
2020-03-02 15:09:00 +0100

    Added some utilities

--- set.sh
+++ set.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
-# You should add following lines to your .zshrc
-### LEARN SCALE ### START ###
+# You should add following line to your .zshrc :
+# Change LEARNSCALE_SET_PATH by the path to this file
+# source LEARNSCALE_SET_PATH
+
+current_path=$0:A
+PATH_TO_THE_LEARN_SCALE_MAIN_PY=current_path+/main.py
 
 if [[ -v PERIOD ]]; then
     # set to half an hour
@@ -8,10 +12,9 @@ if [[ -v PERIOD ]]; then
 fi
 
 if [[ -v periodic_functions ]]; then
-    periodic_functions=($PATH_TO_THE_LEARN_SCALE_MAIN_PY)
+    periodic_functions=(python3 $PATH_TO_THE_LEARN_SCALE_MAIN_PY)
 else
-    periodic_functions+=($PATH_TO_THE_LEARN_SCALE_MAIN_PY)
+    periodic_functions+=(python3 $PATH_TO_THE_LEARN_SCALE_MAIN_PY)
 fi
 
-### LEARN SCALE ### END ###
 

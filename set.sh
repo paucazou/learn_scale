2a7baa09f370fd763a70c47eccbb45c012929b0f
Philippe Aucazou <paucazou@yahoo.fr>
2020-03-05 05:36:02 +0100

    Fixed some bugs

--- set.sh
+++ set.sh
@@ -3,18 +3,15 @@
 # Change LEARNSCALE_SET_PATH by the path to this file
 # source LEARNSCALE_SET_PATH
 
-current_path=$0:A
-PATH_TO_THE_LEARN_SCALE_MAIN_PY=current_path+/main.py
+PATH_TO_THE_LEARN_SCALE_MAIN_PY=${0%/*}/main.py
 
-if [[ -v PERIOD ]]; then
+if [[ ! -v PERIOD ]]; then
     # set to half an hour
-    PERIOD=1800
+    export PERIOD=1800
 fi
 
-if [[ -v periodic_functions ]]; then
-    periodic_functions=(python3 $PATH_TO_THE_LEARN_SCALE_MAIN_PY)
-else
-    periodic_functions+=(python3 $PATH_TO_THE_LEARN_SCALE_MAIN_PY)
-fi
+
+__call_learn_scale () { python3 $PATH_TO_THE_LEARN_SCALE_MAIN_PY }
+periodic_functions+=(__call_learn_scale)
 
 
